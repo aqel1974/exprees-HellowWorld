337bc55ce2388e19cd2f01218f114cbb8be4266e
aqel1974 <81834895+aqel1974@users.noreply.github.com>
2021-04-03 02:18:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hellow World !!!!');
+  res.send('Hellow World 1234');
 });
 
 app.listen(process.env.PORT || 3000);
